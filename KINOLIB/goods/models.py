9c7a6a983a7ca6af0de1c79962e065ca8dfeea11
Kirill <it_kirill2005@mail.ru>
2024-04-27 22:39:43 +0300

    + create bd and models ( 4:06 )

--- KINOLIB/goods/models.py
+++ KINOLIB/goods/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+
+class Products(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    type = models.TextField(blank=True, null=True, verbose_name='Тип')
+    time = models.IntegerField(default=0, verbose_name='Время')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        db_table = 'product'
+
