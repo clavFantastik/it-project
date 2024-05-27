6759bbc70f9ba60a6f4512c16f58b34578232c08
Artyom <fantastik.play.2000@gmail.com>
2024-05-27 17:49:36 +0300

    watch comms

--- KINOLIB/auth_home/views.py
+++ KINOLIB/auth_home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from home.models import Product
+from .models import Message
 
 def ShowAuthHome(request, id):
+    if request.method == 'POST':
+        if request.POST.get('message') and not Message.objects.filter(description=request.POST.get('message')).exists():
+            Message.objects.create(title=request.user.username, description=request.POST.get('message'), product=request.POST.get('product'))
 
+    return render(request, 'film.html', {'product': Product.objects.get(id=str(id)), 'messages': Message.objects.all(), 'str_prod': str(id)})
 
-    return render(request, 'film.html', {'product': Product.objects.get(id = str(id))})
--- KINOLIB/user_sign/urls.py
+++ KINOLIB/user_sign/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path("signup/", views.SignUpPage, name="signup"),
     path("login/", views.LogInPage, name="login"),
     path("logout/", views.LogoutPage, name="logout"),
-    # path("example/", views.FilmPage, name="film"),
-    # path("login/example/", views.FilmPage, name="login_film"),
 ]
