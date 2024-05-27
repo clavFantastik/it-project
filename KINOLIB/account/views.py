11383606c8cef16bde10da3fb44d279cbd33ec7f
siniiy <stayoverboard2@gmail.com>
2024-05-27 15:55:27 +0300

    add info-page

--- KINOLIB/account/views.py
+++ KINOLIB/account/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from home.models import Product
+from .forms import UserEditForm
+
+
+def showInfo(request):
+    if request.method == 'POST' and request.POST.get('id') and not User.objects.filter(username=request.POST.get('username')).exists():
+        user = User.objects.get(pk=request.POST.get('id'))
+        form = UserEditForm(request.POST, instance=user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/profile/info')
+
+    return render(request, 'info.html')
 
 
 def showFavorite(request):
@@ -11,4 +24,7 @@ def showFavorite(request):
             res.append(favorite)
 
     print(res)
-    return render(request, "favorite.html", {"favorites": res})
+    return render(request, 'favorite.html', {'favorites': res})
+
+
+
